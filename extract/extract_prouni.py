2fa23d681c244c871a5cd92b95666df168f2d56c
bianca_maria <bias.marias.ss.pp@gmail.com>
2024-10-09 11:57:56 -0300

    Fix filenames, add curl as web package extractor

--- extract/extract_prouni.py
+++ extract/extract_prouni.py
@@ -2,18 +2,21 @@
 from requests import get
 from pandas import DataFrame, to_numeric, concat
 from bs4 import BeautifulSoup
-from os import path, system, remove, makedirs
+from os import path, system, remove, makedirs, getcwd
 from glob import glob
+from config.config import Config
 
 
 class ProuniExtractor:
   def __init__(self):
-    self.url = None # adicionar self.config
-    self.start_year = None # adicionar self.config
-    self.title = None # adicionar self.config
-    self.limit = None # adicionar self.config
-    self.data_dir = None # adicionar self.config
+    self.config = Config()
+    self.url = self.config.vars.prouni_url
+    self.start_year = self.config.vars.start_year # adicionar self.config
+    self.title = self.config.vars.title # adicionar self.config
+    self.limit = self.config.vars.limit # adicionar self.config
 
+    self.caminho_atual = getcwd()
+    self.data_dir = f'{self.caminho_atual}{self.config.vars.data_dir_prouni}'
 
   def get_links(self):
 
@@ -60,14 +63,16 @@ class ProuniExtractor:
       file = doc["arquivo"]
       urlname = doc["url"]
       year = doc["ano"]
-      destfile = (f'{self.data_dir}/{file}')
-      if destfile in glob(f"{self.data_dir}/*"):
+      destfile = (f'{self.data_dir}\{file}')
+      if destfile in glob(f"{self.data_dir}\*"):
         print("Arquivo já existe")
         continue
 
       os_cmd = (
-            f"wget --limit-rate {self.limit}k "
-            f"--no-check-certificate -c {urlname} -O {destfile}")
+        f"curl --limit-rate {self.limit}K "
+        f"--insecure -C - {urlname} -o {destfile}"
+        )
+
       system(os_cmd)
       if ".zip" in file.lower():
         print("Uncompress ZIP file")
